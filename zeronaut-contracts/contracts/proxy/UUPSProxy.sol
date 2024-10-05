126296068e3b15e2d54e410912f638c14c7b3e56
Alejandro <palebluedot@gmail.com>
2024-10-05 12:57:18 -0300

    Merge AbstractProxy

--- zeronaut-contracts/contracts/proxy/UUPSProxy.sol
+++ zeronaut-contracts/contracts/proxy/UUPSProxy.sol
@@ -1,13 +1,12 @@
 //SPDX-License-Identifier: MIT
 pragma solidity >=0.8.11 <0.9.0;
 
-import "./AbstractProxy.sol";
 import "./ProxyStorage.sol";
 import "../utils/AddressUtil.sol";
 import "../errors/AddressError.sol";
 import {OwnableStorage} from "../ownership/OwnableStorage.sol";
 
-contract UUPSProxy is AbstractProxy, ProxyStorage {
+contract UUPSProxy is ProxyStorage {
     constructor(address firstImplementation, address initialOwner) {
         if (initialOwner == address(0)) {
             revert AddressError.ZeroAddress();
@@ -26,7 +25,36 @@ contract UUPSProxy is AbstractProxy, ProxyStorage {
         _proxyStore().implementation = firstImplementation;
     }
 
-    function _getImplementation() internal view virtual override returns (address) {
+    fallback() external payable {
+        _forward();
+    }
+
+    receive() external payable {
+        _forward();
+    }
+
+    function _forward() internal {
+        address implementation = _getImplementation();
+
+        // solhint-disable-next-line no-inline-assembly
+        assembly {
+            calldatacopy(0, 0, calldatasize())
+
+            let result := delegatecall(gas(), implementation, 0, calldatasize(), 0, 0)
+
+            returndatacopy(0, 0, returndatasize())
+
+            switch result
+            case 0 {
+                revert(0, returndatasize())
+            }
+            default {
+                return(0, returndatasize())
+            }
+        }
+    }
+
+    function _getImplementation() internal view virtual returns (address) {
         return _proxyStore().implementation;
     }
 }
