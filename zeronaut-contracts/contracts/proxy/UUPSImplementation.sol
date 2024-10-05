0f04787687c52dc5bfddd9944f371e72d92beb72
Alejandro <palebluedot@gmail.com>
2024-10-05 13:00:48 -0300

    Simplify UUPSImplementation

--- zeronaut-contracts/contracts/proxy/UUPSImplementation.sol
+++ zeronaut-contracts/contracts/proxy/UUPSImplementation.sol
@@ -1,46 +1,15 @@
 //SPDX-License-Identifier: MIT
 pragma solidity >=0.8.11 <0.9.0;
 
-import "../interfaces/IUUPSImplementation.sol";
 import "../errors/AddressError.sol";
 import "../utils/AddressUtil.sol";
 import "../errors/MiscError.sol";
 import "./ProxyStorage.sol";
 
-abstract contract UUPSImplementation is IUUPSImplementation, ProxyStorage {
-    /**
-     * @inheritdoc IUUPSImplementation
-     */
-    function simulateUpgradeTo(address newImplementation) public override {
-        ProxyStore storage store = _proxyStore();
+event Upgraded(address indexed self, address implementation);
 
-        store.simulatingUpgrade = true;
-
-        address currentImplementation = store.implementation;
-        store.implementation = newImplementation;
-
-        (bool rollbackSuccessful, ) = newImplementation.delegatecall(
-            abi.encodeCall(this.upgradeTo, (currentImplementation))
-        );
-
-        if (!rollbackSuccessful || _proxyStore().implementation != currentImplementation) {
-            revert UpgradeSimulationFailed();
-        }
-
-        store.simulatingUpgrade = false;
-
-        // solhint-disable-next-line reason-string
-        revert();
-    }
-
-    /**
-     * @inheritdoc IUUPSImplementation
-     */
-    function getImplementation() external view override returns (address) {
-        return _proxyStore().implementation;
-    }
-
-    function _upgradeTo(address newImplementation) internal virtual {
+abstract contract UUPSImplementation is ProxyStorage {
+    function upgradeTo(address newImplementation) internal virtual {
         if (newImplementation == address(0)) {
             revert AddressError.ZeroAddress();
         }
@@ -55,25 +24,12 @@ abstract contract UUPSImplementation is IUUPSImplementation, ProxyStorage {
             revert MiscError.NoChange();
         }
 
-        if (!store.simulatingUpgrade && _implementationIsSterile(newImplementation)) {
-            revert ImplementationIsSterile(newImplementation);
-        }
-
         store.implementation = newImplementation;
 
         emit Upgraded(address(this), newImplementation);
     }
 
-    function _implementationIsSterile(
-        address candidateImplementation
-    ) internal virtual returns (bool) {
-        (bool simulationReverted, bytes memory simulationResponse) = address(this).delegatecall(
-            abi.encodeCall(this.simulateUpgradeTo, (candidateImplementation))
-        );
-
-        return
-            !simulationReverted &&
-            keccak256(abi.encodePacked(simulationResponse)) ==
-            keccak256(abi.encodePacked(UpgradeSimulationFailed.selector));
+    function getImplementation() external view returns (address) {
+        return _proxyStore().implementation;
     }
 }
