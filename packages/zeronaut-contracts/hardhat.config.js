d47912568bdce0146f3424cc0ff737ca8ad4011e
Alejandro <palebluedot@gmail.com>
2024-10-14 09:32:59 -0300

    Deploying

--- packages/zeronaut-contracts/hardhat.config.js
+++ packages/zeronaut-contracts/hardhat.config.js
@@ -1,5 +1,5 @@
+require('dotenv').config({ path: '../../.env' });
 require('@nomicfoundation/hardhat-toolbox');
-
 require('./index');
 
 module.exports = {
@@ -11,5 +11,24 @@ module.exports = {
         runs: 200,
       },
     },
+  },
+  defaultNetwork: 'localhost',
+  networks: {
+    localhost: {
+      url: 'http://127.0.0.1:8545',
+    },
+    op_sepolia: {
+      url: 'https://sepolia.optimism.io',
+      accounts: [process.env.PRIVATE_KEY],
+      ignition: {
+        maxFeePerGasLimit: 50_000_000_000n, // 50 gwei
+        maxPriorityFeePerGas: 2_000_000_000n, // 2 gwei
+        gasPrice: 50_000_000_000n, // 50 gwei
+        disableFeeBumping: false,
+      },
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
   },
 };
